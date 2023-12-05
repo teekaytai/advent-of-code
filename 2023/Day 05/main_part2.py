115ea3f14957ef233e27771ad0ce3266e8718380
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-12-05 19:23:17 +1100

    Day 5: Fix off-by-one interval endpoint error

--- 2023/Day 05/main_part2.py
+++ 2023/Day 05/main_part2.py
@@ -3,7 +3,8 @@ from operator import itemgetter
 import sys
 
 seed_ranges = [int(x) for x in input().split()[1:]]
-item_intervals = deque((seed_ranges[i], seed_ranges[i] + seed_ranges[i + 1]) for i in range(0, len(seed_ranges), 2))
+# Convert (start, size) pairs to half-open intervals [start, start + size)
+item_intervals = deque((start, start + size) for start, size in zip(seed_ranges[::2], seed_ranges[1::2]))
 new_item_intervals: list[tuple[int, int]] = []
 
 for line in sys.stdin:
@@ -14,17 +15,17 @@ for line in sys.stdin:
     dst_start, src_start, size = map(int, line.split())
     src_end = src_start + size
     for _ in range(len(item_intervals)):
-        item_lo, item_hi = item_intervals.popleft()
-        if item_hi >= src_start and item_lo <= src_end:
-            overlap_lo = max(item_lo, src_start)
-            overlap_hi = min(item_hi, src_end)
-            new_item_intervals.append((overlap_lo - src_start + dst_start, overlap_hi - src_start + dst_start))
-            if item_lo < src_start:
-                item_intervals.append((item_lo, src_start - 1))
-            if item_hi > src_end:
-                item_intervals.append((src_end + 1, item_hi))
+        item_start, item_end = item_intervals.popleft()
+        if item_end > src_start and item_start < src_end:
+            overlap_start = max(item_start, src_start)
+            overlap_end = min(item_end, src_end)
+            new_item_intervals.append((overlap_start - src_start + dst_start, overlap_end - src_start + dst_start))
+            if item_start < src_start:
+                item_intervals.append((item_start, src_start))
+            if item_end > src_end:
+                item_intervals.append((src_end, item_end))
         else:
-            item_intervals.append((item_lo, item_hi))
+            item_intervals.append((item_start, item_end))
 
 item_intervals += new_item_intervals
 print(min(map(itemgetter(0), item_intervals)))
