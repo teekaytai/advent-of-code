2087fb36c3e22b362992f57186da5aaa22049a6a
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-12-09 22:37:34 +1100

    Day 8: Add golf solutions

---
+++ 2023/Day 08/golf_part2.py
@@ -0,0 +1,10 @@
+import math
+a,_,*L=open(0)
+d={u:(v[1:4],w[:3])for u,_,v,w in map(str.split,L)}
+s=[u for u in d if'B'>u[2]]
+p=1
+for c in s:
+ i=0
+ while'Z'>c[2]:c=d[c][a[i%(len(a)-1)]>'Q'];i+=1
+ p=math.lcm(p,i)
+print(p)
