eb68a7efbd38e33a40d91e8bdea5ef0fd1997f9d
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-12-10 02:52:03 +1100

    Day 9: Use 2D numpy array to neaten polynomial solution

--- 2023/Day 09/polynomial.py
+++ 2023/Day 09/polynomial.py
@@ -1,18 +1,14 @@
+# The process described in the preamble is the Method of Finite Differences,
+# which is used to calculate values of a polynomial.
+# We can obtain a good estimate of the final answer by approximating the polynomials directly
+# and evaluating them to find the next and previous terms of each history.
+
 import numpy as np
 from numpy.polynomial.polynomial import polyfit, polyval
 import sys
 
-# The process described in the preamble is the Method of Finite Differences,
-# which is used to calculate values of a polynomial.
-# We can obtain a good estimate of the final answer by finding the polynomial directly
-# and evaluating it to find the next and previous terms.
-total_part1 = 0
-total_part2 = 0
-for line in sys.stdin:
-    nums = [int(y) for y in line.split()]
-    N = len(nums)
-    coefs = polyfit(x=np.arange(N), y=nums, deg=N-1)
-    total_part1 += round(polyval(N, coefs))
-    total_part2 += round(polyval(-1, coefs))
-print(total_part1)
-print(total_part2)
+ys = np.array([[int(y) for y in line.split()] for line in sys.stdin]).T
+N = ys.shape[0]
+coefs = polyfit(x=np.arange(N), y=ys, deg=N-1)
+print(polyval(N, coefs).round().sum(dtype=int))  # Part 1
+print(polyval(-1, coefs).round().sum(dtype=int))  # Part 2
