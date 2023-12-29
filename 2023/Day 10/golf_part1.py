afe97900b08e483b6b1cf0aacd269b0940791bca
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-12-29 16:43:39 +0800

    Day 10: Greatly improve golf solutions

--- 2023/Day 10/golf_part1.py
+++ 2023/Day 10/golf_part1.py
@@ -1,11 +1,9 @@
-g=[*open(i:=0).read()]
+*g,=open(0).read()
 G=g.index
 W=G('\n')+1
 f=lambda z:{z+d for d in[[-W,W],[1,-1],[-W,1],[-W,-1],[W,-1],[1,W],[]]['|-LJ7F'.find(g[z])]}
-s=G(S:='S')
-q,*Q=[s+d for d in(-W,1,W,-1)if-1<s+d<len(g)and s in f(s+d)],
-while q:
- for z in q:
-  for Z in f(z):Q+=[Z]*(g[Z]!=S);g[z]=S
- q,*Q=Q,;i+=1
-print(i)
+p=s=G(S:='S')
+q,_=[s+d for d in(-W,1,W,-1)if-1<s+d<len(g)and s in f(s+d)]
+i=1
+while q!=s:p,[q]=q,f(q)-{p};i+=1
+print(i//2)
--- 2023/Day 10/golf_part2.py
+++ 2023/Day 10/golf_part2.py
@@ -1,14 +1,9 @@
-L=len(g:=[*open(I:=0).read()])
+*g,=open(I:=0).read()
 G=g.index
-W=G('\n')+1
-B=[0]*L
-D=[{-W,W},{1,-1},{-W,1},{-W,-1},{W,-1},{1,W},{}]
+D=[{W:=G('\n')+1,-W},{1,i:=-1},{-W,1},{-W,-1},{W,-1},{1,W},{}]
 P='|-LJ7F'
 f=lambda z:{z+d for d in D[P.find(g[z])]}
-q,*Q=[s:=G('S')],
-g[s]=P[D.index({d for d in(-W,1,W,-1)if-1<s+d<L and s in f(s+d)})]
-while q:
- for z in q:
-  for Z in f(z):Q+=[Z]*-~-B[Z];B[z]=1
- q,*Q=Q,
-print(sum(1-b&(I:=I^b&(c in'|LJ'))for c,b in zip(g,B)))
+q=Q={s:=G('S')}
+g[s]=P[D.index({d for d in(-W,1,W,-1)if-1<s+d<len(g)and s in f(s+d)})]
+while Q:q|=Q;Q=set.union(*map(f,Q))-q
+print(sum(1-(b:=(i:=i+1)in q)&(I:=I^b&(c in'|LJ'))for c in g))
