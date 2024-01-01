758b95d553e77bdb6e625ae1ed284f2cffc32587
teekaytai <90845551+teekaytai@users.noreply.github.com>
2024-01-01 16:29:18 +0800

    Day 16: Improve golf solutions

--- 2023/Day 16/golf_part1.py
+++ 2023/Day 16/golf_part1.py
@@ -1,5 +1,5 @@
 g=open(0).read()
-W=g.index('\n')+1
+W=g.find('\n')+1
 q=[(0,1)]
-for z,d in q:m=lambda*D:[(z+d,d)for d in D];q+=[p for p in[m(-1,1),*[m(d)]*4,m(-W,W),m(d//-W),m(d*-W),m(d//W),m(d*W)]['-.|/'.find(g[z])*2+d*d%W]if(W*W-W>p[0]>=0and' '<g[p[0]])*~-(p in q)]
+for z,d in q:m=lambda*D:[(z+d,d)for d in D];q+=[p for p in[m(-1,1),*[m(d)]*4,m(-W,W),m(d//-W),m(d*-W),m(d//W),m(d*W)]['-.|/'.find(g[z])*2+d*d%W]if-1<p[0]<W*W*(W-p[0]%W>1)*-~-(p in q)-W]
 print(len({z for z,_ in q}))
--- 2023/Day 16/golf_part2.py
+++ 2023/Day 16/golf_part2.py
@@ -1,8 +1,7 @@
-g=open(0).read()
-W=g.index('\n')+1
-N=W*W-W
+N=len(g:=open(0).read())
+W=g.find('\n')+1
 def f(s):
  q=[s]
- for z,d in q:m=lambda*D:[(z+d,d)for d in D];q+=[p for p in[m(-1,1),*[m(d)]*4,m(-W,W),m(d//-W),m(d*-W),m(d//W),m(d*W)]['-.|/'.find(g[z])*2+d*d%W]if(N>p[0]>=0and' '<g[p[0]])*~-(p in q)]
+ for z,d in q:m=lambda*D:[(z+d,d)for d in D];q+=[p for p in[m(-1,1),*[m(d)]*4,m(-W,W),m(d//-W),m(d*-W),m(d//W),m(d*W)]['-.|/'.find(g[z])*2+d*d%W]if-1<p[0]<N*(W-p[0]%W>1)*-~-(p in q)]
  return len({z for z,_ in q})
 print(max(max(map(f,zip([N-i-2,W*i,i,W*i+W-2],[-W,1,W,-1])))for i in range(W-1)))
