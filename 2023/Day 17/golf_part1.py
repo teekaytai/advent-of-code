8d276f2565203da580a52a58756a9710fa5fb8fb
teekaytai <90845551+teekaytai@users.noreply.github.com>
2024-01-02 02:18:36 +0800

    Day 17: Add golf solutions

---
+++ 2023/Day 17/golf_part1.py
@@ -0,0 +1,7 @@
+from heapq import*
+V={N:=len(g:=open(x:=0).read())}
+q=[(0,0,1,0),(0,0,W:=g.find('\n')+1,0)]
+while q:
+ t,z,d,c=heappop(q);s=z,d,c
+ if{s}-V:V|={s};x=x or(z>N-3)*t;R=W//d;[heappush(q,(t+int(g[Z]),Z,D,C))for Z,D,C in[(z+d,d,c+1),(z+R,R,1),(z-R,-R,1)][c>2:]if-1<Z<N>W-Z%W>1]
+print(x)
