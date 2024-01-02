14f5696d1e2ff414f71eb4db9183a56ead486546
teekaytai <90845551+teekaytai@users.noreply.github.com>
2024-01-03 04:03:27 +0800

    Day 10: Improve golf solutions

--- 2023/Day 10/golf_part1.py
+++ 2023/Day 10/golf_part1.py
@@ -1,9 +1,9 @@
-*g,=open(0).read()
-G=g.index
+g=open(p:=0).read()
+G=g.find
 W=G('\n')+1
-f=lambda z:{z+d for d in[[-W,W],[1,-1],[-W,1],[-W,-1],[W,-1],[1,W],[]]['|-LJ7F'.find(g[z])]}
-p=s=G(S:='S')
-q,_=[s+d for d in(-W,1,W,-1)if-1<s+d<len(g)and s in f(s+d)]
+s=G('S')
+f=lambda z:{z+d for d in[[-W,W],[1,-1],[-W,1],[-W,-1],[W,-1],[1,W],[]]['|-LJ7F'.find(g[s+z])]}
+q,_=[d for d in(-W,1,W,-1)if-1<s+d<len(g)and 0in f(d)]
 i=1
-while q!=s:p,[q]=q,f(q)-{p};i+=1
+while q:p,[q]=q,f(q)-{p};i+=1
 print(i//2)
--- 2023/Day 10/golf_part2.py
+++ 2023/Day 10/golf_part2.py
@@ -1,10 +1,9 @@
-*g,=open(i:=0).read()
-G=g.index
+g=open(0).read()
+G=g.find
 W=G('\n')+1
-f=lambda z:{z+d for d in[[-W,W],[1,-1],[-W,1],[-W,-1],[W,-1],[1,W],[]]['|-LJ7F'.find(g[z])]}
-p=s=G(S:='S')
-q,_=[s+d for d in(-W,1,W,-1)if-1<s+d<len(g)and s in f(s+d)]
-h=lambda:p%W*q//W-q%W*p//W
-A=h()
-while q!=s:p,[q]=q,f(q)-{p};A+=h();i+=1
+s=G('S')
+f=lambda z:{z+d for d in[[-W,W],[1,-1],[-W,1],[-W,-1],[W,-1],[1,W],[]]['|-LJ7F'.find(g[s+z])]}
+q,_=[d for d in(-W,1,W,-1)if-1<s+d<len(g)and 0in f(d)]
+A=p=i=0
+while q:p,[q]=q,f(q)-{p};A+=(p+s%W)//W*2*((s+p)%W-(s+q)%W);i+=1
 print(abs(A//2)-i//2)
