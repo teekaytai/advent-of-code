512cdf09e5e5ac3573a82ac9b565cc24d35f07e3
teekaytai <90845551+teekaytai@users.noreply.github.com>
2024-01-21 18:20:02 +0800

    Day 19: Improve part 2 golf solution

--- 2023/Day 19/golf_part2.py
+++ 2023/Day 19/golf_part2.py
@@ -8,7 +8,7 @@ def f(v,p):
   for l,r in p.values():z*=r-l
   t[0]+=z;return
  *w,V=S(W[v],',')
- for x in w:y,u=S(x,':');c,o,*_=y;n=int(y[2:]);R=n,*p[c],n+1;b='<'<o;f(u,{**p,c:(R[2*b+1],R[2*b])});p[c]=R[b::2]
+ for x in w:y,u=S(x,':');c,o,*_=y;n=int(y[2:]);R=n,*p[c],n+1;b='<'<o;f(u,p|{c:(R[2*b+1],R[2*b])});p[c]=R[b::2]
  f(V,p)
 f('in',{c:(1,4001)for c in'xmas'})
 print(*t)
